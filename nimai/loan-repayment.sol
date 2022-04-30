974597abc873d0e344caeeb6eb9632863dae3f03
Nimai Desai <desainimai@gmail.com>
2022-04-30 14:43:57 -0400

    made changes

--- nimai/loan-repayment.sol
+++ nimai/loan-repayment.sol
@@ -1,20 +1,19 @@
 pragma solidity ^0.5.5;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
+import "./LoanApplication.sol";
 
 contract Interest{
 
     using SafeMath for uint;
-    uint256 mulResult;
-    uint256 mulResult2;
-    uint public MAX_VALUE = 2**256 -1;
+    uint256 interesttopay;
 
     function getResults(uint256 LoanAmount, uint256 interest, uint256 term) public{
-        mulResult = SafeMath.mul(LoanAmount, SafeMath.mul(interest, term));
+        interesttopay = SafeMath.mul(LoanAmount, SafeMath.mul(interest, term));
 
     }
-    function mulResultVal() public view returns(uint256){
-        return mulResult;
+    function interestval() public view returns(uint256){
+        return interesttopay;
     }
 
     }
