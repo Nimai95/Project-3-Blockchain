cb62d200e4f5f819a490bab5b155db7d9ebf753a
tiyub <tiyub@com.com>
2022-04-30 08:22:28 -0400

    commit file prep. for questions regarding web3.eth.ETH.accounts

--- ronald/app.py
+++ ronald/app.py
@@ -9,7 +9,8 @@ from web3 import Web3
 w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
 # import the functions from xro_eth_acc.py
-account = generate_account(w3)
+account_default = generate_account(w3)
+# account_all = list_all_account(w3)
 
 ##########################################################
 # streamlit code
@@ -18,19 +19,24 @@ account = generate_account(w3)
 st.markdown("# xroToken: MetaMask account listing")
 st.text("\n")
 
-# list of ganache accounts
+# list default ganache account
 st.markdown("default ganache blockchain address")
-st.write(account.address)
+st.write(account_default.address)
 st.text("\n")
+
+# # ganache list all accounts
+# st.markdown("default ganache blockchain address")
+# st.write(account_all.address)
+# st.text("\n")
 
 st.markdown(
     """
         to do / can do?
-        * list currently active instead of default acc.  
+        * list currently active instead of default acc.  rr
         * list all accs. 
         * list balances
     """
-    )
+)
 st.text("\n")
 
 # st.markdown("***todo: list all accs.")
@@ -38,9 +44,9 @@ st.text("\n")
 
 # r:    omit streamlit footer
 hide_streamlit_style = """
-            <style>
-            #MainMenu {visibility: visible;}
-            footer {visibility: hidden;}
-            </style>
-            """
+    <style>
+        #MainMenu {visibility: hidden;}
+        footer {visibility: hidden;}
+    </style>
+"""
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
--- ronald/xro_eth_acc.py
+++ ronald/xro_eth_acc.py
@@ -29,7 +29,25 @@ def generate_account(w3):
     private, public = wallet.derive_account("eth")
 
     # Convert private key into an Ethereum account
-    account = Account.privateKeyToAccount(private)
+    account_default = Account.privateKeyToAccount(private)
 
     # Return the account from the function
-    return account
+    return account_default
+
+
+# # function called `list_all_account` that fetches all the accounts in MetaMask
+# def list_all_account(w3):
+#     # Access the mnemonic phrase from the `.env` file
+#     mnemonic = os.getenv("MNEMONIC")
+
+#     # Create Wallet object instance
+#     wallet = Wallet(mnemonic)
+
+#     # Derive Ethereum private key
+#     private, public = wallet.derive_account("eth")
+
+#     # Convert private key into an Ethereum account
+#     account_all = Account.privateKeyToAccount(private)
+
+#     # Return the account from the function
+#     return account_all
