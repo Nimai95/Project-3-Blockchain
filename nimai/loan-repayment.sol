faed71008bc7ddbce19a306397be97e613015ad8
Nimai Desai <desainimai@gmail.com>
2022-04-30 14:36:42 -0400

    third commit

--- nimai/loan-repayment.sol
+++ nimai/loan-repayment.sol
@@ -1,57 +1,20 @@
 pragma solidity ^0.5.5;
 
-contract loan{
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/math/SafeMath.sol";
 
-    address lender;
+contract Interest{
 
-    constructor() public {
-        lender=msg.sender;
+    using SafeMath for uint;
+    uint256 mulResult;
+    uint256 mulResult2;
+    uint public MAX_VALUE = 2**256 -1;
+
+    function getResults(uint256 LoanAmount, uint256 interest, uint256 term) public{
+        mulResult = SafeMath.mul(LoanAmount, SafeMath.mul(interest, term));
+
+    }
+    function mulResultVal() public view returns(uint256){
+        return mulResult;
     }
 
-    struct Loan {
-        address borrower;
-        address token;
-        uint256 amount;
-        bool repaid;
     }
-
-    //mapping (bytes => loan) public loans;
-}  
-
-    //function issue(bytes32 id, address recipient, address token, uint256 value) public returns (bool){
-    //make sure it is the loaner that is loaning
-   // require(msg.sender==loaner,"Loan::issue only loaner can loan");
-    //make sure there isnt already a fund here
-   // require(loans[id].recipient==address(0),"Loan::issue id already exists");
-    //store loan information
-    //loans[id] = Loan({
-    //  recipient: recipient,
-    //  token: token,
-    //  amount: value,
-    //  repaid: false
-    //});
-    //transfer tokens to recipient
-    //IERC20(token).transfer(recipient,value);
-    //fire event for logging
-   // emit Issue(id, recipient, token, value);
-    return true;
-  }
-  //event Issue(bytes32 id, address recipient, address token, uint256 value);
-
-  function repay(bytes32 id) public returns (bool){
-      require(loans[id].borrower!=address(0),);
-      require(loans[id].repaid);
-      loans[id].repaid = true;
-      //transfer funds from the borrower and verfiy
-      uint256 startingBalance = IERC20(loans[id].token).balanceof(address(this));
-      IERC20(loans[id].token).transferFrom(msg.sender, address(this), loans[id].amount);
-        require(
-     (startingBalance+loans[id].amount) == IERC20(loans[id].token).balanceOf(address(this)),
-     "ERC20 Balance did not change correctly"
-    );
-    //emit event for logging
-    emit Repay(id,loans[id].borrower,loans[id].token,loans[id].amount);
-    return true;
-  }
-  event Repay(bytes32 id, address borrower, address token, uint256 value);
-  }
