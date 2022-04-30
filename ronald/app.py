796856eccec3a174427b475249dbce71bac315a0
tiyub <tiyub@com.com>
2022-04-30 06:20:06 -0400

    commit Streamlit local blockchain connection

--- ronald/app.py
+++ ronald/app.py
@@ -1,58 +1,49 @@
-##########################################################
 # Imports
-import os
-import json
 import streamlit as st
-from dataclasses import dataclass
-from typing import Any, List
-from pathlib import Path
-from dotenv import load_dotenv
+
+# Import the functions from ethereum.py
+from xro_eth_acc import generate_account
 from web3 import Web3
-# w3 = Web3(Web3.HTTPProvider('HTTP://127.0.0.1:8545'))
-##########################################################
 
-load_dotenv()
-
-# Define and connect a new Web3 provider
-w3 = Web3(Web3.HTTPProvider(os.getenv("WEB3_PROVIDER_URI")))
+w3 = Web3(Web3.HTTPProvider('http://127.0.0.1:8545'))
 
 ##########################################################
-# Contract Helper function:
-# 1. Loads the contract once using cache
-# 2. Connects to the contract using the contract address and ABI
-##########################################################
 
-# Cache the contract on load
-@st.cache(allow_output_mutation=True)
-
-# Define the load_contract function
-def load_contract():
-
-    # Load Art Gallery ABI
-    with open(Path('contracts/compiled/xroToken_abi.json')) as f:
-        certificate_abi = json.load(f)
-
-    # Set the contract address (this is the address of the deployed contract)
-    contract_address = os.getenv("SMART_CONTRACT_ADDRESS")
-
-    # Get the contract
-    contract = w3.eth.contract(
-        address=contract_address,
-        abi=certificate_abi
-    )
-    # Return the contract from the function
-    return contract
-
-
-# Load the contract
-contract = load_contract()
-
+# Import the functions from ethereum.py
+account = generate_account(w3)
 
 ##########################################################
 # Streamlit code
 ##########################################################
 
-# Streamlit application headings
-st.markdown("# xroToken")
-st.markdown("by The Graces xro Smart Contracts will work in this UI")
+# Streamlit response test
+st.markdown("# xroToken: MetaMask account listing")
 st.text("\n")
+
+# list of Ganache accounts
+# Write the Ethereum account address to the Streamlit page
+st.markdown("default ganache blockchain address")
+st.write(account.address)
+st.text("\n")
+
+st.markdown(
+    """
+        to do / can do?
+        * list currently active instead of default acc.  
+        * list all accs. 
+        * list balances
+    """
+    )
+st.text("\n")
+
+# st.markdown("***todo: list all accs.")
+# st.text("\n")
+
+# r:    omit Streamlit footer
+hide_streamlit_style = """
+            <style>
+            #MainMenu {visibility: visible;}
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
